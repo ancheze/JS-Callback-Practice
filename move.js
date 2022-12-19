e659fe80f7dff2a67ad494ae12ad2e1483c59776
ancheze <keilasanchezerazo@MacBook-Pro.local>
2022-12-19 13:13:15 -0500

    save one from assignment

--- move.js
+++ move.js
@@ -1,3 +1,7 @@
+let direction = null;
+let x = 100;
+let y = 250;
+
 function move(element) {
     element.style.position = 'fixed'
 
@@ -6,7 +10,36 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        function moveCharacter (){
+            if(direction === 'west') {
+                x = x - 1 
+            }
+            if(direction === 'north') {
+                y = y + 1 
+            }
+            if(direction === 'east') {
+                x = x + 1 
+            }
+            if(direction === 'south') {
+                y = y - 1 
+            }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        }
+
+        
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
---
+++ newCharacter.js
@@ -0,0 +1,21 @@
+
+const character = newImage('assets/green-character/static.gif')
+let direction = null;
+move(character).to(100, 250)
+
+const character = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
+move(character).to(100, 250)
+
+// function newItem(url){
+    // let item = newImage(url)
+    // item.addEventListener('click', () => {
+        // item.remove()
+        // let inventoryItem = document.createElement('img')
+        // inventoryItem.src = url;
+        // inventory.append(inventoryItem)
+    // })
+    // return item
+// }
